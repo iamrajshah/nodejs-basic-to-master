dd9a9de1e4862b934bf1701d193a34bf5a422b11
iamrajshah <shahrajesh2113@yahoo.com>
2025-06-18 17:51:27 +0530

    Added event loop files and episodes

---
+++ Practice/eventLoop/process-inside-fs.js
@@ -0,0 +1,35 @@
+/**
+ * Last line of the file.
+ * nextTick
+ * Promise
+ * Timer expired
+ * setImmediate
+ * File Reading CB
+ * 2nd nextTick
+ * 2nd setImmediate
+ * 2nd timer
+ */
+
+const fs = require("fs");
+
+setImmediate(() => console.log("setImmediate"));
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+Promise.resolve("Promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", () => {
+  // Here most important is that this will call at last not like outside..
+  // Reason is immediate is present in XX stage and that will execute first and at last
+  // this below timer
+  setTimeout(() => console.log("2nd timer"), 0);
+
+  process.nextTick(() => console.log("2nd nextTick"));
+
+  setImmediate(() => console.log(" 2nd setImmediate"));
+
+  console.log("File Reading CB");
+});
+process.nextTick(() => console.log("nextTick"));
+
+console.log("Last line of the file.");
---
+++ Practice/eventLoop/with-promise-and-process.js
@@ -0,0 +1,31 @@
+// a=100
+// Last line of the file.
+// process.nextTick
+// Promise
+// timer
+// setImmediate
+// File Reading CB
+
+const fs = require("fs");
+const a = 100;
+
+setImmediate(() => console.log("setImmediate"));
+
+// High priority
+Promise.resolve("Promise").then(console.log);
+
+fs.readFile("../simpletext.txt", "utf8", () => {
+  console.log("File Reading CB");
+});
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+// Highest priority
+process.nextTick(() => console.log("process.nextTick"));
+
+function printA() {
+  console.log("a=", a);
+}
+
+printA();
+console.log("Last line of the file.");
